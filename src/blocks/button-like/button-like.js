7c3dd47a76c51c6ec8f6e19a57310822cb144993
v-ershov <sully.weber@gmail.com>
2020-02-18 16:30:11 +0300

    Edit button-like.js (change architecture)

--- src/blocks/button-like/button-like.js
+++ src/blocks/button-like/button-like.js
@@ -1,91 +1,77 @@
-(function likeButton() {
-  // ---------------
-  // ---VARIABLES---
-  // ---------------
-
-  let isClickable = true; // если true, то кнопка доступна для клика
-
-  const classButton = 'button-like'; // основной класс кнопки
-  const classButtonActive = `${classButton}--active`; // класс модификатора кнопки, обозначающий, что кнопка активна
-  const classNumbers = `${classButton}__numbers`; // класс элемента кнопки, обозначающий контейнер для числовых значений
-
-  const animTime = 300; // продолжительность css-анимации в миллисекундах
-
-  // ---------------
-  // ---FUNCTIONS---
-  // ---------------
-
-  // увеличивает значение кнопки на 1
-  function increase(button) {
-    isClickable = false;
-
-    const elemNumbers = button.firstChild;
-    const elemNumber = elemNumbers.firstChild;
-
-    elemNumbers.innerHTML = '';
-
-    elemNumbers.insertAdjacentHTML('afterbegin', elemNumber.outerHTML);
-    elemNumber.innerHTML = parseInt(elemNumber.innerHTML, 10) + 1;
-    elemNumbers.insertAdjacentHTML('afterbegin', elemNumber.outerHTML);
-
-    const classIncrease = `${classNumbers}--increase`;
-    const classAnimIncrease = `${classNumbers}--anim-increase`;
-
-    elemNumbers.classList.add(classIncrease);
-
-    setTimeout(() => {
-      elemNumbers.classList.add(classAnimIncrease);
-    }, 1);
-
-    setTimeout(() => {
-      elemNumbers.lastChild.remove();
-      elemNumbers.classList.remove(classIncrease, classAnimIncrease);
-      isClickable = true;
-    }, animTime);
+class ButtonLike {
+  constructor(node) {
+    this.node = node;
+    this.numbers = node.querySelector('.button-like__numbers');
+    this.number = node.querySelector('.button-like__number');
+    this.classActive = 'button-like--active';
+    this.classIncrease = 'button-like__numbers--increase';
+    this.classAnimIncrease = 'button-like__numbers--anim-increase';
+    this.classAnimDecrease = 'button-like__numbers--anim-decrease';
+    this.animTime = 300;
+    this.isClickable = true;
+    this._addEventListeners();
   }
 
-  // уменьшает значение кнопки на 1
-  function reduce(button) {
-    isClickable = false;
-
-    const elemNumbers = button.firstChild;
-    const elemNumber = elemNumbers.firstChild;
-
-    elemNumbers.innerHTML = '';
-
-    elemNumbers.insertAdjacentHTML('beforeend', elemNumber.outerHTML);
-    elemNumber.innerHTML = parseInt(elemNumber.innerHTML, 10) - 1;
-    elemNumbers.insertAdjacentHTML('beforeend', elemNumber.outerHTML);
-
-    const classAnimReduce = `${classNumbers}--anim-reduce`;
-
-    setTimeout(() => {
-      elemNumbers.classList.add(classAnimReduce);
-    }, 1);
-
-    setTimeout(() => {
-      elemNumbers.firstChild.remove();
-      elemNumbers.classList.remove(classAnimReduce);
-      isClickable = true;
-    }, animTime);
-  }
-
-  // ------------
-  // ---EVENTS---
-  // ------------
-
-  // возникает при клике по кнопке
-  document.querySelectorAll(`.${classButton}`).forEach((button) => {
-    button.addEventListener('click', () => {
-      if (isClickable) {
-        if (button.classList.contains(classButtonActive)) {
-          button.classList.remove(classButtonActive);
-          reduce(button);
-        } else {
-          button.classList.add(classButtonActive);
-          increase(button);
-        }
-      }
+  // регистрирует обработчики событий
+  _addEventListeners() {
+    this.node.addEventListener('click', () => {
+      this._switchState();
     });
-  });
-}());
+  }
+
+  // уменьшает количество лайков на единицу
+  _decrease() {
+    const newNumber = this.numbers.lastChild.cloneNode(true);
+    newNumber.textContent = parseInt(newNumber.textContent, 10) - 1;
+
+    this.numbers.appendChild(newNumber);
+
+    setTimeout(() => {
+      this.numbers.classList.add(this.classAnimDecrease);
+    }, 1);
+
+    setTimeout(() => {
+      this.numbers.classList.remove(this.classAnimDecrease);
+      this.numbers.firstChild.remove();
+    }, this.animTime);
+  }
+
+  // увеличивает количество лайков на единицу
+  _increase() {
+    const newNumber = this.numbers.firstChild.cloneNode(true);
+    newNumber.textContent = parseInt(newNumber.textContent, 10) + 1;
+
+    this.numbers.insertBefore(newNumber, this.numbers.firstChild);
+    this.numbers.classList.add(this.classIncrease);
+
+    setTimeout(() => {
+      this.numbers.classList.add(this.classAnimIncrease);
+    }, 1);
+
+    setTimeout(() => {
+      this.numbers.classList.remove(this.classIncrease, this.classAnimIncrease);
+      this.numbers.lastChild.remove();
+    }, this.animTime);
+  }
+
+  // активирует или деактивирует кнопку в зависимости от текущего состояния
+  _switchState() {
+    if (this.isClickable) {
+      this.isClickable = false;
+
+      if (this.node.classList.contains(this.classActive)) {
+        this.node.classList.remove(this.classActive);
+        this._decrease();
+      } else {
+        this.node.classList.add(this.classActive);
+        this._increase();
+      }
+
+      setTimeout(() => {
+        this.isClickable = true;
+      }, this.animTime);
+    }
+  }
+}
+
+document.querySelectorAll('.button-like').forEach((node) => new ButtonLike(node));
