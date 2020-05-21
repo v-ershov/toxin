8c51bdea7ca119396f2c00ab374729b6958e83e3
v-ershov <sully.weber@gmail.com>
2020-05-21 16:10:28 +0300

    Add new block — slider-range

---
+++ src/blocks/slider-range/slider-range.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/slider';
+import 'jquery-ui/themes/base/slider.css';
+import 'jquery-ui/themes/base/theme.css';
+
+class SliderRange {
+  constructor($node) {
+    this.$node = $node;
+    this._createSlider();
+  }
+
+  // создаёт слайдер
+  _createSlider() {
+    const label = this.$node.find('.label__secondary');
+    const input = this.$node.find('.slider-range__input');
+    const slider = this.$node.find('.slider-range__slider');
+    const unit = slider.data('unit');
+
+    slider.slider({
+      animate: 'fast',
+      classes: {
+        'ui-slider': '',
+        'ui-slider-handle': '',
+        'ui-slider-range': '',
+      },
+      max: slider.data('max'),
+      min: slider.data('min'),
+      range: true,
+      step: slider.data('step'),
+      values: [slider.data('start'), slider.data('end')],
+
+      slide(_e, ui) {
+        const value = `${ui.values[0].toLocaleString('ru')}${unit} - ${ui.values[1].toLocaleString('ru')}${unit}`;
+        label.text(value);
+        input.val(value);
+      },
+    });
+  }
+}
+
+$('.slider-range').each((_i, node) => new SliderRange($(node)));
