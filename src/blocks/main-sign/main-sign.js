c91f874cceb5ef46c8ac32b78f401986991c4f36
v-ershov <sully.weber@gmail.com>
2020-06-28 21:35:07 +0300

    Add new block — main-sign

---
+++ src/blocks/main-sign/main-sign.js
@@ -0,0 +1,27 @@
+class Dropdown {
+  constructor(node) {
+    this.node = node;
+    this.parallaxSpeed = 2.5;
+    this._findNodes();
+    this._addEventListeners();
+  }
+
+  // находит указанные дочерние элементы корневого элемента
+  _findNodes() {
+    this.background = this.node.querySelector('.sign__background');
+  }
+
+  // регистрирует обработчики событий
+  _addEventListeners() {
+    window.addEventListener('scroll', () => {
+      this._createParallax();
+    });
+  }
+
+  // создаёт параллакс-эффект для фонового изображения
+  _createParallax() {
+    this.background.style.transform = `translateY(${window.pageYOffset / this.parallaxSpeed}px)`;
+  }
+}
+
+document.querySelectorAll('.sign').forEach((node) => new Dropdown(node));
