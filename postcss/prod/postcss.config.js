b8a5f735324a31c3bfda790df462c14f23eacfc9
v-ershov <sully.weber@gmail.com>
2020-08-04 11:25:10 +0300

    Remove css-mqpacker

--- postcss/prod/postcss.config.js
+++ postcss/prod/postcss.config.js
@@ -1,11 +1,9 @@
 const autoprefixer = require('autoprefixer');
-const cssMqpacker = require('css-mqpacker');
 const cssnano = require('cssnano');
 
 module.exports = {
   plugins: [
     autoprefixer,
-    cssMqpacker,
     cssnano({
       preset: [
         'default', {
