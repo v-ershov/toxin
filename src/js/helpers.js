cab302161e3fca04265c0296ea9cdd3eff617509
v-ershov <sully.weber@gmail.com>
2020-08-11 17:25:24 +0300

    helpers.js — add new function «animate»

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,6 +1,31 @@
+// создаёт анимацию с помощью requestAnimationFrame
+// https://javascript.info/js-animation#structured-animation
+function animate({ duration, timing, draw }) {
+  const start = performance.now();
+
+  requestAnimationFrame(function step(time) {
+    let timeFraction = (time - start) / duration;
+
+    if (timeFraction > 1) {
+      timeFraction = 1;
+    }
+
+    const progress = timing(timeFraction);
+
+    draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(step);
+    }
+  });
+}
+
 // возвращает ширину вертикальной полосы прокрутки главного окна
-const getScrollbarWidth = () => window.innerWidth - document.body.clientWidth;
+function getScrollbarWidth() {
+  return window.innerWidth - document.body.clientWidth;
+}
 
 export default {
+  animate,
   getScrollbarWidth,
 };
