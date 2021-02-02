bd5fe81f0bf247377f36d590e2ec62353eca5eae
v-ershov <sully.weber@gmail.com>
2021-02-02 15:45:34 +0300

    slider-range — refactoring with new standards

--- src/blocks/slider-range/slider-range.ts
+++ src/blocks/slider-range/slider-range.ts
@@ -3,9 +3,9 @@ import 'jquery-ui/themes/base/slider.css';
 import 'jquery-ui-touch-punch/jquery.ui.touch-punch.min';
 
 interface ISliderRangeElements {
-  $label: JQuery<HTMLElement>;
-  $input: JQuery<HTMLElement>;
-  $slider: JQuery<HTMLElement>;
+  label?: HTMLLabelElement;
+  input: HTMLInputElement;
+  slider: HTMLDivElement;
 }
 
 class SliderRange {
@@ -23,54 +23,67 @@ class SliderRange {
 
   constructor(root: HTMLElement) {
     this._root = root;
-    this._elements = this._getElements();
+    this._elements = this._findElements();
 
-    this._initSlider();
+    this._initJqueryUiSlider();
   }
 
   // -----------------------
   // --- PRIVATE METHODS ---
   // -----------------------
 
-  // возвращает элементы слайдера
-  private _getElements(): ISliderRangeElements {
+  // находит и возвращает элементы слайдера
+  private _findElements(): ISliderRangeElements {
     return {
-      $label: $(this._root).find('.label__secondary'),
-      $input: $(this._root).find('.slider-range__input'),
-      $slider: $(this._root).find('.slider-range__slider'),
+      label: this._root.querySelector('.label__secondary') as HTMLLabelElement,
+      input: this._root.querySelector('.slider-range__input') as HTMLInputElement,
+      slider: this._root.querySelector('.slider-range__slider') as HTMLDivElement,
     };
   }
 
-  // инициализирует слайдер
-  private _initSlider(): void {
-    const { $label, $input, $slider } = this._elements;
+  // инициализирует экземпляр плагина jquery-ui-slider
+  private _initJqueryUiSlider(): void {
+    const {
+      label,
+      input,
+      slider,
+    } = this._elements;
 
-    $slider.slider({
+    const data = slider.dataset;
+
+    $(slider).slider({
       animate: 'fast',
       classes: {
         'ui-slider': '',
         'ui-slider-handle': '',
         'ui-slider-range': '',
       },
-      max: +$slider.data('max'),
-      min: +$slider.data('min'),
+      max: +(data.max as string),
+      min: +(data.min as string),
       range: true,
-      step: +$slider.data('step'),
-      values: [+$slider.data('start'), +$slider.data('end')],
+      step: +(data.step as string),
+      values: [+(data.start as string), +(data.end as string)],
 
       slide(_e, ui) {
         const values = ui.values as number[];
-        const unit = $slider.data('unit') as string;
+        const unit = data.unit as string;
 
-        const start = `${values[0].toLocaleString('ru')}${unit}`;
-        const end = `${values[1].toLocaleString('ru')}${unit}`;
+        const start = `${values[0].toLocaleString('ru-RU')}${unit}`;
+        const end = `${values[1].toLocaleString('ru-RU')}${unit}`;
         const value = `${start} - ${end}`;
 
-        $label.text(value);
-        $input.val(value);
+        input.value = value;
+
+        if (label) {
+          label.textContent = value;
+        }
       },
     });
   }
 }
 
-document.querySelectorAll('.slider-range').forEach((el) => new SliderRange(el as HTMLElement));
+export default function render(): void {
+  document.querySelectorAll('.slider-range').forEach((el) => new SliderRange(el as HTMLElement));
+}
+
+render();
