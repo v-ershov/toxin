6c465e1903061e2d8686771af4aad8b4ec52b3d8
v-ershov <sully.weber@gmail.com>
2021-04-02 13:10:13 +0300

    paths.ts — remove unnecessary paths

--- webpack/paths.ts
+++ webpack/paths.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 
 const src = path.resolve('src');
-const dist = path.resolve('dist');
 const postcss = path.resolve('postcss');
 
 const paths = {
@@ -18,14 +17,8 @@ const paths = {
     pug: {
       pages: `${src}\\pug\\pages`,
     },
-    scss: {
-      _functions: `${src}\\scss\\_functions.scss`,
-      _mixins: `${src}\\scss\\_mixins.scss`,
-      _vars: `${src}\\scss\\_vars.scss`,
-    },
     _index: `${src}\\index.ts`,
   },
-  dist,
   postcss: {
     _dev: `${postcss}\\postcss.dev.js`,
     _prod: `${postcss}\\postcss.prod.js`,
